cd65270b1c43a5854a15dc137e5471ac54cdbe9c
Victor Dumbrava <vdxcoder123@gmail.com>
2018-02-09 10:32:29 +0200

    Update Constants.py

--- Constants.py
+++ Constants.py
@@ -1,8 +1,8 @@
 G = 6.674 * (10 ** (-11))
 c = 299792458
 g = 9.80665
-π = 3.14159265358979323846
+Pi = 3.14159265358979323846
 e = 2.71828182845904523536
 Infty = float('inf')
-NegInfty =  float('-inf')
+NegInfty = float('-inf')
 x = __import__('sympy').Symbol('x')
