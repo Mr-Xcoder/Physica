dfd851cc51d0d387ed040b7d94d166976cfa02be
Victor Dumbrava <vdxcoder123@gmail.com>
2018-02-09 10:32:13 +0200

    Update Executor.py

--- Executor.py
+++ Executor.py
@@ -1,8 +1,7 @@
 from Functions import *
 from Transpiler import *
 
-
 if __name__ == "__main__":
     program = open(__import__("sys").argv[1], "r", encoding='utf-8').read()
-    code = transpile(program)
+    code = Transpiler().transpile(program)
     exec(code)
