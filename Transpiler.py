5f572f6d4365e3d8366f9096656494f1be9266aa
Victor Dumbrava <vdxcoder123@gmail.com>
2018-02-09 10:31:56 +0200

    Update Transpiler.py

--- Transpiler.py
+++ Transpiler.py
@@ -1,53 +1,59 @@
-def transpile(code):
-    result = ""
-    pause = 0
-    for index, character in enumerate(code):
-        if pause:
-            pause -= 1
-            continue
-        if code[: index].count('"') % 2:
-            result += character
-        else:
-            if character == "[":
-                result += "("
-            elif character == "]":
-                result += ")"
-            elif character == ";":
-                result += ","
-            elif character == ",":
-                result += ";"
-            elif character == "^":
-                result += "**"
-            elif code[index:index+2] == "**":
-                pause = 1
-                result += "^"
-            elif character == "≤":
-                result += "<="
-            elif character == "≥":
-                result += ">="
-            elif character == "√":
-                result += "Root"
-            elif character == "µ":
-                result += "FrictionCoefficient"
-            elif character == "∂":
-                result += "Differentiate"
-            elif character == "∫":
-                result += "Integrate"
-            elif character == "⟶":
-                result += "lambda "
-            elif character == "~":
-                result += "::"
-            elif character == "{":
-                result += "["
-            elif character == "}":
-                result += "]"
-            elif character == "…":
-                result += "Range"
-            elif code[index : index+5] == "Until":
-                pause = 4
-                result += "while not"
-            elif code[index : index+6] == "import":
-                raise ImportError("Python imports are specifically disallowed")
-            else:
+class Transpiler:
+    @staticmethod
+    def transpile(code: str) -> str:
+        result = ""
+        pause = 0
+        for index, character in enumerate(code):
+            if pause:
+                pause -= 1
+                continue
+            if (code[: index].count("'") - code[: index].count("\\'")) % 2:
                 result += character
-    return result
+            else:
+                if character == "[":
+                    result += "("
+                elif character == "]":
+                    result += ")"
+                elif character == ";":
+                    result += ","
+                elif character == ",":
+                    result += ";"
+                elif character == "^":
+                    result += "**"
+                elif code[index:index + 2] == "**":
+                    pause = 1
+                    result += "^"
+                elif character == "≤":
+                    result += "<="
+                elif character == "≥":
+                    result += ">="
+                elif character == "√":
+                    result += "Root"
+                elif character == "µ":
+                    result += "FrictionCoefficient"
+                elif character == "∂":
+                    result += "Differentiate"
+                elif character == "∫":
+                    result += "Integrate"
+                elif character == "⟶":
+                    result += "lambda "
+                elif character == "~":
+                    result += "::"
+                elif character == "{":
+                    result += "["
+                elif character == "}":
+                    result += "]"
+                elif character == "…":
+                    result += "Range"
+                elif code[index: index + 5] == "Until":
+                    pause = 4
+                    result += "while not"
+                elif code[index: index + 6] == "import":
+                    raise ImportError("Python imports are specifically disallowed")
+                else:
+                    result += character
+        return result
+
+    @staticmethod
+    def format_list(item: list) -> str:
+        return "{" + "; ".join(map(repr, item)) + "}"
