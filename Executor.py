49a3d0543296fd718655e38ea6d2a738db2151f5
Victor Dumbrava <vdxcoder123@gmail.com>
2018-02-25 12:32:32 +0200

    Update Executor.py

--- Executor.py
+++ Executor.py
@@ -1,5 +1,5 @@
 from Functions import *
-from Transpiler import *
+from ParsingInfix import *
 
 if __name__ == "__main__":
     program = open(__import__("sys").argv[1], "r", encoding='utf-8').read()
