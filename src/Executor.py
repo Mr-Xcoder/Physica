d074b496671d10d551e51d22c5846830903854c2
Victor Dumbrava <vdxcoder123@gmail.com>
2018-05-27 19:44:47 +0300

    Update Executor.py

--- src/Executor.py
+++ src/Executor.py
@@ -1,7 +1,6 @@
-from Functions import *
-from ParsingInfix import *
+import Parser
 
 if __name__ == "__main__":
-    program = open(__import__("sys").argv[1], "r", encoding='utf-8').read()
-    code = Transpiler().transpile(program)
-    exec(code)
+    code = open(__import__("sys").argv[1], "r", encoding='utf-8').read()
+    parsed_code = Parser.Parser().parse(code)
+    exec("from Functions import *\n\n" + parsed_code)
